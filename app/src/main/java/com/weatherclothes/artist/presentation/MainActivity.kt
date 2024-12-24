bc3f9551f9d0e6ec0ff93e34674e5d60bebd3424
Ivan Blinov <iv2002blin@gmail.com>
2024-12-24 09:05:29 +0700

    Task-5: Implemented CreatePermission Fragment

--- app/src/main/java/com/weatherclothes/artist/presentation/MainActivity.kt
+++ app/src/main/java/com/weatherclothes/artist/presentation/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.weatherclothes.artist.presentation
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.weatherclothes.artist.R
 import com.weatherclothes.artist.databinding.ActivityMainBinding
 
+private const val TAG = "MyLog"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -16,8 +25,32 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            Log.d(TAG, "version = 35")
+
+            window.statusBarColor = this.getColor(R.color.statusBar)
+
+            ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(0, systemBars.top, 0, 0)
+
+//                v.updatePadding(top = insets.systemWindowInsetTop)
+
+                insets
+            }
+
+        } else {
+            Log.d(TAG, "version < 35")
+        }
+
+//        window.decorView.setOnApplyWindowInsetsListener(null)
+
+//        window.insetsController?.systemBarsBehavior =
+//            WindowInsetsController.BEHAVIOR_SHOW_BARS_BY_TOUCH
 
         navController =
             (supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment).navController
